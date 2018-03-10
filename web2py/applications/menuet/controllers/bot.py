6688f55d52793874f3b3f8804fbcaac5626f3fc2
Proger666 <knorra@mail.ru>
2018-03-10 12:05:15 +0300

    init

--- web2py/applications/menuet/controllers/bot.py
+++ web2py/applications/menuet/controllers/bot.py
@@ -1,7 +1,7 @@
 # -*- coding:utf8 -*-
 # !/usr/bin/env python
 
-
+@request.restful()
 def botWebhook():
-    logger.info("request from bot" + str(request))
+    logger.warn("request from bot" + str(request))
     return locals()
