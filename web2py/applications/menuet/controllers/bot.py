c8c753b1e63f43f5992b31c991cf63774e725c31
Proger666 <knorra@mail.ru>
2018-03-10 12:43:57 +0300

    init

--- web2py/applications/menuet/controllers/bot.py
+++ web2py/applications/menuet/controllers/bot.py
@@ -2,17 +2,24 @@
 # !/usr/bin/env python
 from gluon.contrib import simplejson
 
+
 @request.restful()
 def webhook():
     def POST(*args, **vars):
         logger.error(str(request))
         print(str(request))
-        return simplejson.dumps({'speech':"lol"})
+        return simplejson.dumps({
+            "speech": 'lol',
+            "displayText": 'nah',
+            # "data": data,
+            # "contextOut": [],
+            "source": "apiai-weather-webhook-sample"
+        })
 
     def GET(*args, **vars):
         logger.error(str(request))
         print(str(request))
-        return simplejson.dumps({'speech':"lol"})
+        return simplejson.dumps({'speech': "lol"})
 
     logger.warn("request from bot" + str(request))
     return locals()
