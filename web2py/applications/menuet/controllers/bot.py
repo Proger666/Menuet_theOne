7331d58f00c09fc4eb08e81eea01c77177b1d131
Proger666 <knorra@mail.ru>
2018-03-10 12:22:03 +0300

    init

--- web2py/applications/menuet/controllers/bot.py
+++ web2py/applications/menuet/controllers/bot.py
@@ -1,14 +1,18 @@
 # -*- coding:utf8 -*-
 # !/usr/bin/env python
+from gluon.contrib import simplejson
+
 
 def botWebhook():
     def POST(*args, **vars):
         logger.error(str(request))
         print(str(request))
+        return simplejson.dumps({'speech':"lol"})
 
     def GET(*args, **vars):
         logger.error(str(request))
         print(str(request))
+        return simplejson.dumps({'speech':"lol"})
 
     logger.warn("request from bot" + str(request))
     return locals()
