fe2c007cd900d067bc9aded381f1ab0446233d5c
Proger666 <knorra@mail.ru>
2018-03-11 17:56:23 +0300

    init

--- web2py/applications/menuet/controllers/bot.py
+++ web2py/applications/menuet/controllers/bot.py
@@ -3,16 +3,16 @@
 from gluon.contrib import simplejson
 import re
 
+
 def get_user_location_google(text):
     if text == None:
         logger.error('faled to fullfill user location - parameter is empty ' + logUser_and_request())
-    return re.search('\d+\.\d+\,\d+\.\d+',text).group(0)
-
+    return re.search('\d+\.\d+\,\d+\.\d+', text).group(0)
 
 
 def get_user_info(userQuery):
     # fast fail if empty req
-    if len(userQuery) <1:
+    if len(userQuery) < 1:
         logger.error("Fullfilment failed for bot request" + logUser_and_request())
     # lets get current user context
     userInfo = userQuery['payload']['data']['message']['chat']
@@ -26,12 +26,14 @@ def get_user_info(userQuery):
     user_f_name = userInfo.first_name
     user_l_name = userInfo.last_name
     if user_context == None:
-        new_user = db.bot_user_context.insert(f_username=user_name,f_user_id=user_id,f_first_name=user_f_name,f_last_name=user_l_name,f_last_loc=user_last_loc)
-        db.commit()
-
+        new_user = db.t_bot_user_context.insert(f_username=user_name, f_user_id=user_id, f_first_name=user_f_name,
+                                                f_last_name=user_l_name, f_last_loc=user_last_loc)
+    else:
+        db(db.t_bot_user_context.id == user_context.id).update(f_last_loc=user_last_loc)
+    db.commit()
 
     userContext = {'userName': user_name,
-                   'user_id':user_id,
+                   'user_id': user_id,
                    'user_last_location': user_last_loc}
 
     return userContext
@@ -41,13 +43,7 @@ def get_user_info(userQuery):
 def webhook():
     def POST(*args, **vars):
         user_context = get_user_info(request.vars.originalDetectIntentRequest)
-        return simplejson.dumps({
-            "speech": 'lol',
-            "displayText": 'nah',
-            # "data": data,
-            # "contextOut": [],
-            "source": "apiai-weather-webhook-sample"
-        })
+        return simplejson.dumps(user_context)
 
     logger.warn("request from bot" + str(request))
     return locals()
