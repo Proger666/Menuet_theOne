cc9905d4159dbf70c0e4f0995f63daefddbe79f7
Proger666 <knorra@mail.ru>
2018-05-21 00:33:03 +0300

    init

--- web2py/applications/menuet/models/internal_tools.py
+++ web2py/applications/menuet/models/internal_tools.py
@@ -1,11 +1,13 @@
 def checkIfExist(*args):
     '''Test if arguments are exists via len, etc None'''
     for arg in args:
-        arg = str(arg.encode('utf-8'))
-        if arg is None  or len(arg) == 0 or arg == '' or arg == " ":
-            return False
-        else:
-            return True
+        try:
+            if arg is None  or len(arg) == 0 or arg == '' or arg == " ":
+                return False
+            else:
+                return True
+        except AttributeError:
+            pass
 
 
 
