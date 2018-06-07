ec3e919e80a0e4c045241a60e07a8b1c3f28c8ad
Proger666 <knorra@mail.ru>
2018-06-07 12:10:00 +0300

    init

--- web2py/applications/menuet/controllers/bot.py
+++ web2py/applications/menuet/controllers/bot.py
@@ -137,29 +137,32 @@ def search_by_name(query, weight, rest1k, rests_item):
     # result obj
 
     result = []
-
+    exact_match = False
     for item in rests_item:
         item = Storage(item)
         # remove excessive spaces
         if re.sub(' +', ' ', item.item_name.lower()) == query:
             create_result_obj(item, rest1k, result, weight)
+            exact_match = True
             break
-        if len(result) == 0:
-            # lets try search word by word until fail
-            # lets try search via regex in full string
-            compile = re.compile(query.encode('utf-8'))
+
+        if exact_match == True:
+            break
+        # lets try search word by word until fail
+        # lets try search via regex in full string
+        compile = re.compile(query.encode('utf-8'))
+        if compile.search(item.item_name.lower().encode('utf-8')) is not None:
+            create_result_obj(item, rest1k, result, weight)
+            break
+        query_list = query.split()
+        while len(query_list) > 1:
+            query_list = query_list[:-1]
+            compile = re.compile(" ".join(query_list).encode('utf-8'))
             if compile.search(item.item_name.lower().encode('utf-8')) is not None:
                 create_result_obj(item, rest1k, result, weight)
                 break
-            query_list = query.split()
-            while len(query_list) > 1:
-                query_list = query_list[:-1]
-                compile = re.compile(" ".join(query_list).encode('utf-8'))
-                if compile.search(item.item_name.lower().encode('utf-8')) is not None:
-                    create_result_obj(item, rest1k, result, weight)
-                    break
-                if len(result) != 0:
-                    break
+            if len(result) != 0:
+                break
     return result
 
 
