9e84f826764cb03c4e5636b8099ce806830ecfcc
Proger666 <knorra@mail.ru>
2018-06-07 11:59:28 +0300

    init

--- web2py/applications/menuet/controllers/bot.py
+++ web2py/applications/menuet/controllers/bot.py
@@ -154,7 +154,7 @@ def search_by_name(query, weight, rest1k, rests_item):
             query_list = query.split()
             while len(query_list) > 0:
                 query_list = query_list[:-1]
-                compile = re.compile(" ".join(query_list))
+                compile = re.compile(" ".join(query_list).encode('utf-8'))
                 if compile.search(item.item_name.lower().encode('utf-8')) is not None:
                     create_result_obj(item, rest1k, result, weight)
                     break
