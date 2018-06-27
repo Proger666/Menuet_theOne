d8a60496a8bcc53ed3b9e749a22f02546d87a8de
vdenisov <j>
2018-06-27 17:08:37 +0300

    asd

--- web2py/applications/menuet/controllers/bot.py
+++ web2py/applications/menuet/controllers/bot.py
@@ -250,8 +250,8 @@ def search_by_name(query, weight, rest1k, rests_item, query_id):
             logger.warning("we got regular as %s", regex_str)
             compile = re.compile(regex_str)
             # re.compile("".join(map((lambda x: "((\\s | ^){x}\\S * ?\\s)|(\\S*?{x}(\\s | $))".format(x=x)), clean_query)))
-            logger.warning("current item to analyze s%", str(item.item_name.lower()))
-            if compile.search(item.item_name.lower()) is not None:
+            logger.warning("current item to analyze s%", str(item.item_name.lower().encode('utf-8')))
+            if compile.search(item.item_name.lower().encode('utf-8')) is not None:
                 create_result_obj(item, rest1k, result, weight, 40)
 
             item_time = datetime.datetime.now() - start
